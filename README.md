3afbdc25ea83a3a317eabf2febceffda3f76b713
Paris Bland <paris@pariselectra.com>
2022-09-30 22:49:33 -0500

    <added README and images>

--- README.md
+++ README.md
@@ -1,1 +1,38 @@
 # JavaScript-Window-Edit
+
+## Description
+This project was the third challenge I completed during a coding bootcamp with the University of Texas at Austin. The purpose of the project was to add functionality to a password generator. To start, the HTML and CSS elements were written in already. There was some basic Javascript given as a starting place as well- the starting Javascript code defined the special characters, numbers, and letters that could be used as a wordbank for the password generator. Additionally, the JavaScript code also provided a path to output the function into a predefined box on the screen.
+
+This project combined all of the basic elements of JavaScript and serves as a demonstration of my knowledge of functions, arrays, objects, iterators, for loops, and JavaScript's connection to CSS/HTML code.
+
+The overall task of the project was to use window prompts to allow the user to input choices. The user had the option to decide if they wanted numbers, lowercase letters, uppercase letters, or special characters in their code. The user also had the ability to set the desired length of their password (as long as it was between 8 and 128 characters). My goal as a developer was to take the five different pieces of selection criteria from the user and output a randomly generated password that fit their criteria.
+
+In order to complete this project, I first pseudo coded the task out. I wrote out the path I wanted the user to take to answer the questions and made sure to store all of the user perferences as variables. Next, I wrote a code that randomly selected a password given each of their variable criteria. The options for numbers, special characters, lowercase, and uppercase letters were all stored as an array. I used the .concat method to string different arrays together based off what the user selected. With four possible selectors, that means there were fifteen possible option combinations. I stored each one of these options as a different array. Essentially, the user selects how large they want their "word bank" of random numbers, letters, and characters to be. Next, the function iterates through that array and randomly selects a password.
+
+## Installation
+
+-Live Site:
+
+-JavaScript file:
+
+## Usage
+![Selector 1](images/Screen%20Shot%202022-09-30%20at%2010.12.12%20PM.png)
+![Selector 2](images/Screen%20Shot%202022-09-30%20at%2010.12.26%20PM.png)
+![Selector 3](images/Screen%20Shot%202022-09-30%20at%2010.12.36%20PM.png)
+![Selector 4](images/Screen%20Shot%202022-09-30%20at%2010.12.49%20PM.png)
+![Selector 5](images/Screen%20Shot%202022-09-30%20at%2010.12.58%20PM.png)
+![Result](images/Screen%20Shot%202022-09-30%20at%2010.13.09%20PM.png)
+
+## License
+
+## Features
+
+-Set Password Length
+
+-Select uppercase preference
+
+-Select lowercase preference
+
+-Select special character preference
+
+-Select number preference
--- script.js
+++ script.js
@@ -1,61 +1,26 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-var lowercaseChar = "abcdefghijklmnopqrstuvwxyz";
-var uppercaseChar = "ABCDEFGHIJKLMNOPQRSTUZWXYZ";
-var numericalChar = "0123456789";
-var specialChar = "!#$%&*";
 
 // Write password to the #password input 
+
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
+
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
 
 
-
-
-
-var lowercase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
-var uppercase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
-
-var generatePassword = function() {
-    var userChoiceOne = window.prompt("How long would you like your password to be? (must be between 8 and 128 characters");
-
-
-var index = Math.floor(Math.random() * lowercase.length);
-var passwordOutput = lowercase[index];
-
-window.alert("Your password is " + passwordOutput);
-
-if (userchoice > 128) { 
-    window.alert("Invalid") 
-}
-else if (userchoice < 8) {
-    window.alert("Invalid")
-}
-else {
-    window.alert(passwordOutput)
-}
-
-generatePassword ();
-
-
-
-
-
-
 var lowercase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
 var uppercase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
 var specialCharacter = ["!","@","#","$","%","^","&","*","<",">","?"];
-var numbers = [1,2,3,4,5,6,7,8,9,0];
-var generatedPassword = '';
+var numbers = ["1","2","3","4","5","6","7","8","9","0"];
+var generatedPassword = "";
 
 var firstOptionSequence = lowercase.concat(uppercase,specialCharacter,numbers);
 var secondOptionSequence = lowercase.concat(uppercase,specialCharacter);
@@ -73,7 +38,8 @@ var thirteenthOptionSequence = uppercase;
 var fourteenthOptionSequence = specialCharacter;
 var fifteenthOptionSequence = numbers;
 
-var passwordGen = function(){
+
+var generatePassword = function() {
     var userLength = window.prompt("How long would you like your password to be? (Must be between 8 and 128 characters)");
     if (userLength > 128) {
         return;
@@ -87,13 +53,127 @@ var passwordGen = function(){
     var userWantsSpecialCharacters = window.prompt("Would you like special characters in your password? (y/n)");
     var userWantsNumbers = window.prompt("Would you like numbers in your password? (y/n)");
 
-    if (userWantsLowercase === "y" && userWantsUppercase === "y" && userWantsSpecialCharacters === "y" && userWantsNumbers === "y") {
+    if ((userWantsLowercase === "y" && userWantsUppercase === "y") && (userWantsSpecialCharacters === "y" && userWantsNumbers === "y")) {
         for (i=0 ; i < userLength ; i++) {
-            Math.floor(Math.random()*firstOptionSequence.length+1)
-            generatedPassword = firstOptionSequence[i];
+            var randomIndex = Math.floor(Math.random()*firstOptionSequence.length);
+            generatedPassword += firstOptionSequence[randomIndex];
         }
+        console.log(generatedPassword);
         return generatedPassword;
     }
+    else if ((userWantsLowercase === "y" && userWantsUppercase === "y") && (userWantsSpecialCharacters === "y" && userWantsNumbers === "n")) {
+        for (i=0 ; i < userLength ; i++) {
+            var randomIndex = Math.floor(Math.random()*secondOptionSequence.length);
+            generatedPassword += secondOptionSequence[randomIndex];
+        }
+        console.log(generatedPassword);
+        return generatedPassword;
+    }
+    else if ((userWantsLowercase === "y" && userWantsUppercase === "y") && (userWantsSpecialCharacters === "n" && userWantsNumbers === "y")) {
+        for (i=0 ; i < userLength ; i++) {
+            var randomIndex = Math.floor(Math.random()*thirdOptionSequence.length);
+            generatedPassword += thirdOptionSequence[randomIndex];
+        }
+        console.log(generatedPassword);
+        return generatedPassword;
+    }
+    else if ((userWantsLowercase === "n" && userWantsUppercase === "y") && (userWantsSpecialCharacters === "y" && userWantsNumbers === "y")) {
+        for (i=0 ; i < userLength ; i++) {
+            var randomIndex = Math.floor(Math.random()*fourthOptionSequence.length);
+            generatedPassword += fourthOptionSequence[randomIndex];
+        }
+        console.log(generatedPassword);
+        return generatedPassword;
+    }
+    else if ((userWantsLowercase === "y" && userWantsUppercase === "n") && (userWantsSpecialCharacters === "y" && userWantsNumbers === "y")) {
+        for (i=0 ; i < userLength ; i++) {
+            var randomIndex = Math.floor(Math.random()*fifthOptionSequence.length);
+            generatedPassword += fifthOptionSequence[randomIndex];
+        }
+        console.log(generatedPassword);
+        return generatedPassword;
+    }
+    else if ((userWantsLowercase === "y" && userWantsUppercase === "y") && (userWantsSpecialCharacters === "n" && userWantsNumbers === "n")) {
+        for (i=0 ; i < userLength ; i++) {
+            var randomIndex = Math.floor(Math.random()*sixthOptionSequence.length);
+            generatedPassword += sixthOptionSequence[randomIndex];
+        }
+        console.log(generatedPassword);
+        return generatedPassword;
+    }
+    else if ((userWantsLowercase === "y" && userWantsUppercase === "n") && (userWantsSpecialCharacters === "y" && userWantsNumbers === "n")) {
+        for (i=0 ; i < userLength ; i++) {
+            var randomIndex = Math.floor(Math.random()*seventhOptionSequence.length);
+            generatedPassword += seventhOptionSequence[randomIndex];
+        }
+        console.log(generatedPassword);
+        return generatedPassword;
+    }
+    else if ((userWantsLowercase === "y" && userWantsUppercase === "n") && (userWantsSpecialCharacters === "n" && userWantsNumbers === "y")) {
+        for (i=0 ; i < userLength ; i++) {
+            var randomIndex = Math.floor(Math.random()*eighthOptionSequence.length);
+            generatedPassword += eighthOptionSequence[randomIndex];
+        }
+        console.log(generatedPassword);
+        return generatedPassword;
+    }
+    else if ((userWantsLowercase === "n" && userWantsUppercase === "y") && (userWantsSpecialCharacters === "y" && userWantsNumbers === "n")) {
+        for (i=0 ; i < userLength ; i++) {
+            var randomIndex = Math.floor(Math.random()*ninthOptionSequence.length);
+            generatedPassword += ninthOptionSequence[randomIndex];
+        }
+        console.log(generatedPassword);
+        return generatedPassword;
+    }
+    else if ((userWantsLowercase === "n" && userWantsUppercase === "y") && (userWantsSpecialCharacters === "n" && userWantsNumbers === "y")) {
+        for (i=0 ; i < userLength ; i++) {
+            var randomIndex = Math.floor(Math.random()*tenthOptionSequence.length);
+            generatedPassword += tenthOptionSequence[randomIndex];
+        }
+        console.log(generatedPassword);
+        return generatedPassword;
+    }
+    else if ((userWantsLowercase === "n" && userWantsUppercase === "n") && (userWantsSpecialCharacters === "y" && userWantsNumbers === "y")) {
+        for (i=0 ; i < userLength ; i++) {
+            var randomIndex = Math.floor(Math.random()*eleventhOptionSequence.length);
+            generatedPassword += eleventhOptionSequence[randomIndex];
+        }
+        console.log(generatedPassword);
+        return generatedPassword;
+    }
+    else if ((userWantsLowercase === "y" && userWantsUppercase === "n") && (userWantsSpecialCharacters === "n" && userWantsNumbers === "n")) {
+        for (i=0 ; i < userLength ; i++) {
+            var randomIndex = Math.floor(Math.random()*twelevthOptionSequence.length);
+            generatedPassword += twelevthOptionSequence[randomIndex];
+        }
+        console.log(generatedPassword);
+        return generatedPassword;
+    }
+    else if ((userWantsLowercase === "n" && userWantsUppercase === "y") && (userWantsSpecialCharacters === "n" && userWantsNumbers === "n")) {
+        for (i=0 ; i < userLength ; i++) {
+            var randomIndex = Math.floor(Math.random()*thirteenthOptionSequence.length);
+            generatedPassword += thirteenthOptionSequence[randomIndex];
+        }
+        console.log(generatedPassword);
+        return generatedPassword;
+    }
+    else if ((userWantsLowercase === "n" && userWantsUppercase === "n") && (userWantsSpecialCharacters === "y" && userWantsNumbers === "n")) {
+        for (i=0 ; i < userLength ; i++) {
+            var randomIndex = Math.floor(Math.random()*fourteenthOptionSequence.length);
+            generatedPassword += fourteenthOptionSequence[randomIndex];
+        }
+        console.log(generatedPassword);
+        return generatedPassword;
+    }
+    else if ((userWantsLowercase === "n" && userWantsUppercase === "n") && (userWantsSpecialCharacters === "n" && userWantsNumbers === "y")) {
+        for (i=0 ; i < userLength ; i++) {
+            var randomIndex = Math.floor(Math.random()*fifteenthOptionSequence.length);
+            generatedPassword += fifteenthOptionSequence[randomIndex];
+        }
+        console.log(generatedPassword);
+        return generatedPassword;
+    }
+    else {
+        return;
+    }
 }
-
-passwordGen();
